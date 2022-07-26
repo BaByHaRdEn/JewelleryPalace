56c711c8939a9e39126f44f050705e7c7b011fc7
BaByHaRdEn <mangergeorgepraise@gmail.com>
2022-07-26 15:22:45 +0100

    night mode

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 const card= document.querySelectorAll(".card");
+const modeBtn= document.getElementById("night-mode-toggle")
 
 let cards=[...card];
 cards.forEach((c,i)=>{
@@ -8,4 +9,13 @@ cards.forEach((c,i)=>{
     card[i].addEventListener('mouseout', ()=>{
         card[i].firstElementChild.style.display="none";
     })
+})
+
+modeBtn.addEventListener("click", ()=>{
+    if(document.body.style.backgroundColor = "black"){
+    document.body.style.backgroundColor = "white";
+    }else{
+        document.body.style.backgroundColor = "black";
+    }
+    
 })
