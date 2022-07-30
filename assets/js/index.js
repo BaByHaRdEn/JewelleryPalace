bcfa4e4df4f1777c3ba3c8389bba19df3107b4af
BaByHaRdEn <mangergeorgepraise@gmail.com>
2022-07-30 14:28:30 +0100

    Style update

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,3 +17,12 @@ modeBtn.addEventListener("click", ()=>{
     let btn=document.getElementById("toggle")
     btn.classList.toggle("fa-toggle-on")
 })
+function scrolll() {
+    let left = document.getElementById("scroll-cards");
+    left.scrollBy(500, 0)
+}
+
+function scrollr() {
+    let right = document.getElementById("scroll-cards");
+    right.scrollBy(-500, 0)
+}
