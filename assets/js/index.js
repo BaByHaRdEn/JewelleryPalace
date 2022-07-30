592f7beb38b89f2493d95c6064c8d2099629fa70
BaByHaRdEn <mangergeorgepraise@gmail.com>
2022-07-30 18:49:22 +0100

    Total update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 const card= document.querySelectorAll(".card");
 const modeBtn= document.getElementById("night-mode-toggle")
 
+//categories
 let cards=[...card];
 cards.forEach((c,i)=>{
     cards[i].addEventListener("mouseover", ()=>{
@@ -11,18 +12,20 @@ cards.forEach((c,i)=>{
     })
 })
 
+//Light and dark mode
 modeBtn.addEventListener("click", ()=>{
     let dark =document.getElementById("light");
     dark.toggleAttribute("disabled")
     let btn=document.getElementById("toggle")
     btn.classList.toggle("fa-toggle-on")
 })
+// arrow keys
 function scrolll() {
     let left = document.getElementById("scroll-cards");
-    left.scrollBy(500, 0)
+    left.scrollBy(300, 0)
 }
 
 function scrollr() {
     let right = document.getElementById("scroll-cards");
-    right.scrollBy(-500, 0)
+    right.scrollBy(-300, 0)
 }
